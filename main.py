fcd3e66f91210272b81dd9df2e5ebb894493e1db
Vlad <Meshkov Vladislav>
2022-05-06 15:53:49 +0300

    rm comment

--- main.py
+++ main.py
@@ -53,6 +53,6 @@ def lalala(message):
                     bot.send_message(message.chat.id, answer, parse_mode = 'HTML')
 #RUN
 #bot.polling(none_stop=True)
-'''
+
 if __name__ == '__main__':
     server.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
