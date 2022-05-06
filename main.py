622d32a460506145d234c25201ff914444e1a4a6
Vlad <Meshkov Vladislav>
2022-05-06 13:27:08 +0300

    app_url

--- main.py
+++ main.py
@@ -5,8 +5,8 @@ import PHRASES
 import os
 from flask import Flask, request
 
-
-
+TOKEN = config.TOKEN
+APP_URL = f'https://reviewshybot.herokuapp.com/{TOKEN}'
 bot = telebot.TeleBot(config.TOKEN)
 server = Flask(__name__)
 engine = Engine()
@@ -19,17 +19,17 @@ def start(message):
 def echo(message):
     bot.reply_to(message, message.text)
 
-@server.route('/')
-def webhook():
-    bot.remove_webhook()
-    bot.set_webhook(url=APP_URL)
-    return '!', 200
-
 @server.route('/' + config.TOKEN, methods=['POST'])
 def get_message():
     json_string = request.get_data().decode('utf-8')
     update = telebot.types.Update.de_json(json_string)
     bot.process_new_updates([update])
+
+@server.route('/')
+def webhook():
+    bot.remove_webhook()
+    bot.set_webhook(url=APP_URL)
+    return '!', 200
 
 
 '''@bot.message_handler(content_types=['text'])
