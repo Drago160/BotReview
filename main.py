e304f12099c5c526c27380b48eb7a35dcab8b9ef
Vlad <Meshkov Vladislav>
2022-05-06 20:29:15 +0300

    letter in main

--- main.py
+++ main.py
@@ -2,4 +2,4 @@ import src.bot
 import os
 
 if __name__ == '__main__':
-    src.Bot.server.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
+    src.bot.server.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
